a0a181a1e812218590faf8c2c8189c0ea907c0ef
Diogo Alves <diogo.alves.ti@gmail.com>
2021-05-17 13:05:41 -0300

    Corrige bug ao tentar fechar a sidebar em algumas telas

--- gerenciador/static/js/base_site.js
+++ gerenciador/static/js/base_site.js
@@ -1,7 +1,5 @@
-window.onload = function() {
-    var btnSidebarCollapser = document.getElementById("sidebarCollapser");
-    var sidebar = document.getElementById("sidebar");
-    btnSidebarCollapser.onclick = function() {
-        sidebar.classList.toggle("active");
-    }
+var btnSidebarCollapser = document.getElementById("sidebarCollapser");
+var sidebar = document.getElementById("sidebar");
+btnSidebarCollapser.onclick = function() {
+    sidebar.classList.toggle("active");
 }
--- gerenciador/static/js/modalDelete.js
+++ gerenciador/static/js/modalDelete.js
@@ -1,14 +1,12 @@
-window.onload = function() {
-    var modalDelete = document.getElementById('modalDelete');
-    modalDelete.addEventListener('show.bs.modal', function (event) {
-        var modalTrigger = event.relatedTarget
+var modalDelete = document.getElementById('modalDelete');
+modalDelete.addEventListener('show.bs.modal', function (event) {
+    var modalTrigger = event.relatedTarget
 
-        var modalForm = modalDelete.querySelector('form')
-        var modalModelName = modalDelete.querySelector('.model-name')
-        var modalObjectDescription = modalDelete.querySelector('.object-description')
+    var modalForm = modalDelete.querySelector('form')
+    var modalModelName = modalDelete.querySelector('.model-name')
+    var modalObjectDescription = modalDelete.querySelector('.object-description')
 
-        modalForm.action = modalTrigger.getAttribute('data-bs-url');
-        modalModelName.textContent = modalTrigger.getAttribute('data-bs-model');
-        modalObjectDescription.textContent = modalTrigger.getAttribute('data-bs-object');
-    })
-}
+    modalForm.action = modalTrigger.getAttribute('data-bs-url');
+    modalModelName.textContent = modalTrigger.getAttribute('data-bs-model');
+    modalObjectDescription.textContent = modalTrigger.getAttribute('data-bs-object');
+})
